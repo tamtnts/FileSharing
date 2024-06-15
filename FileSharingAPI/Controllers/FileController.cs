61b03c6c4ddbb2a016bee95abbe67fe5cd485dc5
tamtnts <tamtnts@gmail.com>
2024-06-16 00:38:46 +0700

    create repo

---
+++ FileSharingAPI/Controllers/FileController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileSharingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+    }
+}
